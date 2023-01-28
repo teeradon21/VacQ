5aff00b764eb42db66c6606121d9d376f425c72c
teeradon21 <teeradon21@outlook.com>
2023-01-28 21:13:58 +0700

    REST API

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+
+const app=express();
+
+const hospitals = require('./routes/hospitals')
+app.use('/api/v1/hospitals',hospitals)
+
+// app.get('/',(req,res) => {
+//     res.status(200).json({success:"true", data:{id:1}});
+// })
+
+
+const PORT=process.env.PORT || 5000;
+app.listen(PORT,console.log('Server running in ', process.env.NODE_ENV, ' mode on port ',PORT));
