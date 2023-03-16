03150f7c0fee67cb3385fcf8ee9e613aacb5db64
teeradon21 <teeradon21@outlook.com>
2023-03-16 13:21:26 +0700

    Add Appointment

--- controllers/hospitals.js
+++ controllers/hospitals.js
@@ -1,12 +1,74 @@
-const Hospital = require('../models/Hospital')
+const Hospital = require('../models/Hospital');
+const { param } = require('../routes/hospitals');
 
 //@desc     Get all hospitals
 //@route    GET /api/v1/hospitals 
 //@access   Public
 exports.getHospitals= async (req,res,next)=>{
     try{
-        const hospitals = await Hospital.find();
-        res.status(200).json({success:true, count:hospitals.length, data:hospitals});
+        let query;
+        //Copy req.query
+        const reqQuery = {...req.query};
+
+        //Fields to exclude
+        const removeFields = ['select','sort','page','limit'];
+
+        //Loop over remove fields and delete them from reqQuery
+        removeFields.forEach(param=>delete reqQuery[param]);
+        //console.log(reqQuery);
+        
+        //Create query string
+        let queryStr = JSON.stringify(req.query);
+
+        //Create operators ($gt, $gte, etc)
+        queryStr = queryStr.replace(/\b(gt|get|lt|lte|in)\b/, match=>`$${match}`);
+
+        //finding resource
+        query = Hospital.find(JSON.parse(queryStr)).populate('appointments');
+
+        //Select Fields
+        if(req.query.select){
+            const fields = req.query.select.split(',').join(' ');
+            query = query.select(fields);
+        }
+        //Sort
+        if(req.query.sort){
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy);
+        } else{
+            query=query.sort('-createdAt');
+        }
+
+        //Pagination
+        const page = parseInt(req.query.page,10) || 1;
+        const limit = parseInt(req.query.limit,10) || 25;
+        const startIndex = (page-1)*limit;
+        const endIndex = page*limit;
+        const total = await Hospital.countDocuments();
+
+        query=query.skip(startIndex).limit(limit);
+
+        //Executing query
+        const hospitals = await query;
+
+        //Pagination result
+        const pagination = {};
+
+        if (endIndex<total){
+            pagination.next={
+                page:page+1,
+                limit
+            }
+        }
+
+        if (startIndex>0){
+            pagination.prev={
+                page:page-1,
+                limit
+            }
+        }
+        
+        res.status(200).json({success:true, count:hospitals.length, pagination, data:hospitals});
     } catch(err){
         res.status(400).json({success:false});
     }
@@ -64,10 +126,11 @@ exports.updateHospital= async (req,res,next)=>{
 //@access   Private
 exports.deleteHospital = async (req,res,next)=>{
     try{
-        const hospital = await Hospital.findByIdAndDelete(req.params.id);
+        const hospital = await Hospital.findById(req.params.id);
         if(!hospital){
             return res.status(400).json({success:false});
         }
+        hospital.remove();
         res.status(200).json({success:true, data:{}});
     } catch(err){
         res.status(400).json({success:false});
--- server.js
+++ server.js
@@ -12,6 +12,7 @@ connectDB();
 //Route files
 const hospitals = require('./routes/hospitals');
 const auth = require('./routes/auth');
+const appointments = require('./routes/appointments');
 
 const app=express();
 
@@ -23,6 +24,7 @@ app.use(cookieParser());
 
 app.use('/api/v1/hospitals',hospitals);
 app.use('/api/v1/auth',auth);
+app.use('/api/v1/appointments',appointments);
 
 // app.get('/',(req,res) => {
 //     res.status(200).json({success:"true", data:{id:1}});
