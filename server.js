2faa60895d2d683dd51793b2937daaf147711385
teeradon21 <teeradon21@outlook.com>
2023-04-10 14:34:50 +0700

    security&openAPI

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 const cors = require("cors");
 
-const mongoSanitize = require('express-mongo-sanitize');
-const helmet=require('helmet');
-const xss=require('xss-clean');
-const rateLimit=require('express-rate-limit');
+const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const rateLimit = require("express-rate-limit");
+
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 //Load env vars
 dotenv.config({ path: "./config/config.env" });
@@ -20,7 +23,6 @@ const hospitals = require("./routes/hospitals");
 const auth = require("./routes/auth");
 const appointments = require("./routes/appointments");
 
-
 const app = express();
 
 app.use(cors());
@@ -31,21 +33,36 @@ app.use(express.json());
 //Cookie parser
 app.use(cookieParser());
 
-//Sanitize data 
+//Sanitize data
 app.use(mongoSanitize());
 
-//Set security headers 
+//Set security headers
 app.use(helmet());
 
-//Prevent XSS attacks 
+//Prevent XSS attacks
 app.use(xss());
 
-//Rate Limiting 
-const limiter=rateLimit({ 
-  windowsMs:10*60*1000,//10 mins 
-  max:100
-}); 
+//Rate Limiting
+const limiter = rateLimit({
+  windowsMs: 10 * 60 * 1000, //10 mins
+  max: 100,
+});
 app.use(limiter);
+
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Library API",
+      version: "1.0.0",
+      description: "A simple Express VacQ API",
+    },
+    servers: [{ url: "http://localhost:5000/api/v1" }],
+  },
+  apis: ["./routes/*.js"],
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use("/api/v1/hospitals", hospitals);
 app.use("/api/v1/auth", auth);
